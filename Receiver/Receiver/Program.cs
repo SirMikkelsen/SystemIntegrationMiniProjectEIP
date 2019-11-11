122d92c5419e6db5d3a38aa05dddaba218e05848
Gordon <gordonmikkelsen87@gmail.com>
2019-11-11 21:35:17 +0100

    Implemented RabbitMQ in Receiver

--- Receiver/Receiver/Program.cs
+++ Receiver/Receiver/Program.cs
@@ -3,19 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
-namespace Receiver
+namespace Customer
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+                    Console.WriteLine();
+                    Console.WriteLine(" Select a for hotel and hit enter");
+                    Console.WriteLine();
+                    Console.WriteLine(" select b for room and hit enter");
+                };
+                channel.BasicConsume(queue: "hello",
+                    autoAck: true,
+                    consumer: consumer);
+
+
+                while (true)
+                {
+                    var input = Console.ReadLine();
+                    Console.WriteLine();
+                    if (input == "a")
+                    {
+
+                        Console.WriteLine("convertet hotel from xml to csv");
+                        break;
+                    }
+                    if (input == "b")
+                    {
+                        Console.WriteLine("convertet room data from xml to csv");
+                        break;
+                    }
+
+                    Console.WriteLine("wrong input");
+                    break;
+                }
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
         }
     }
 }
